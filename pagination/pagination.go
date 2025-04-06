0171053e45e1b992737412db8060c45bf81f3f23
Joshua Ganter <mail@joshuaganter.de>
2025-04-06 21:58:16 +0200

    refactor: Remove duplicate for loops

--- pagination/pagination.go
+++ pagination/pagination.go
@@ -6,38 +6,37 @@ import (
 	"strings"
 )
 
+func getPaginationRange(fromPage int, toPage int, currentPage int) []string {
+	result := make([]string, 0, (toPage-fromPage)+1)
+	for page := fromPage; page <= toPage; page++ {
+		if page == currentPage {
+			result = append(result, fmt.Sprintf("(%d)", page))
+			continue
+		}
+		result = append(result, strconv.Itoa(page))
+	}
+	return result
+}
+
 func GetPagination(currentPage int, totalPages int) string {
-	pagination := make([]string, 0, 7)
 
 	if totalPages <= 7 {
-		for page := 1; page <= totalPages; page++ {
-			if page == currentPage {
-				pagination = append(pagination, fmt.Sprintf("(%d)", page))
-				continue
-			}
-			pagination = append(pagination, strconv.Itoa(page))
-		}
-	} else if currentPage <= 4 {
-		for page := 1; page <= 5; page++ {
-			if page == currentPage {
-				pagination = append(pagination, fmt.Sprintf("(%d)", page))
-				continue
-			}
-			pagination = append(pagination, strconv.Itoa(page))
-		}
-		pagination = append(pagination, "...", strconv.Itoa(totalPages))
-	} else if totalPages-currentPage <= 3 {
-		pagination = append(pagination, "1", "...")
-		for page := totalPages - 4; page <= totalPages; page++ {
-			if page == currentPage {
-				pagination = append(pagination, fmt.Sprintf("(%d)", page))
-				continue
-			}
-			pagination = append(pagination, strconv.Itoa(page))
-		}
-	} else {
-		return fmt.Sprintf("1 ... %d (%d) %d ... %d", currentPage-1, currentPage, currentPage+1, totalPages)
+		return strings.Join(getPaginationRange(1, totalPages, currentPage), " ")
 	}
 
-	return strings.Join(pagination, " ")
+	if currentPage <= 4 {
+		pagination := make([]string, 0, 7)
+		pagination = append(pagination, getPaginationRange(1, 5, currentPage)...)
+		pagination = append(pagination, "...", strconv.Itoa(totalPages))
+		return strings.Join(pagination, " ")
+	}
+
+	if totalPages-currentPage <= 3 {
+		pagination := make([]string, 0, 7)
+		pagination = append(pagination, "1", "...")
+		pagination = append(pagination, getPaginationRange(totalPages-4, totalPages, currentPage)...)
+		return strings.Join(pagination, " ")
+	}
+
+	return fmt.Sprintf("1 ... %d (%d) %d ... %d", currentPage-1, currentPage, currentPage+1, totalPages)
 }
