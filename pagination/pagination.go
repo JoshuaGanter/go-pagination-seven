c2e3f014284f5604d64bb287da9ed5ab2da88472
Joshua Ganter <mail@joshuaganter.de>
2025-04-06 17:34:11 +0200

    feat: Implement pagination function for one parameter-set (first red-green-refactor cycle)

--- pagination/pagination.go
+++ pagination/pagination.go
@@ -1,1 +1,5 @@
 package pagination
+
+func GetPagination(currentPage int, totalPages int) string {
+	return "1 (2) 3 4 5"
+}
--- pagination/pagination_test.go
+++ pagination/pagination_test.go
@@ -1,1 +1,19 @@
 package pagination_test
+
+import (
+	"testing"
+
+	"github.com/JoshuaGanter/go-pagination-seven/pagination"
+)
+
+func TestGetPagination(t *testing.T) {
+	currentPage := 2
+	totalPages := 5
+	expected := "1 (2) 3 4 5"
+
+	actual := pagination.GetPagination(currentPage, totalPages)
+
+	if actual != expected {
+		t.Errorf("GetPagination(%d, %d) returned %q, expected %q", currentPage, totalPages, actual, expected)
+	}
+}
