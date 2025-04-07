ecb335b5cf8852d888c83eeba0f990ba5b0025c0
Joshua Ganter <mail@joshuaganter.de>
2025-04-07 20:33:17 +0200

    feat: Implement CLI usage

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/JoshuaGanter/go-pagination-seven/pagination"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	if len(os.Args) != 3 {
+		fmt.Println("Usage: pagination <currentPage> <totalPages>")
+		return
+	}
+
+	currentPage, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	totalPages, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+
+	paginationString, err := pagination.GetPagination(currentPage, totalPages)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(paginationString)
 }
